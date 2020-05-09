8ffbb09bdb78fe8b558e548422865288e231d59c
jonmariano21 <jonmariano21@gmail.com>
2020-05-09 13:14:02 -0700

    Chat room functionality

--- public/js/main.js
+++ public/js/main.js
@@ -23,7 +23,9 @@ if('undefined' == typeof username || !username){
     username = 'Anonymous_' + Math.random();
 }
 
-$('#messages').append('<h4>' + username + '</h4>');
+//$('#messages').append('<h4>' + username + '</h4>');
+
+var chat_room = 'One_Room_To_Rule_Them_All';
 
 /* Connect to the socket server */
 
@@ -31,4 +33,53 @@ var socket = io.connect();
 
 socket.on('log', function(array) {
     console.log.apply(console, array);
+});
+
+socket.on('join_room_response', function(payload) {
+    if(payload.result == 'fail') {
+        alert(payload.message);
+        return;
+    }
+    $('#messages').append('<p> New user joined the room: ' +payload.username+ '</p>');
+});
+
+
+
+socket.on('send_message_response', function(payload) {
+    if(payload.result == 'fail') {
+        alert(payload.message);
+        return;
+    }
+
+    console.log('BEFOER APPEND!!');
+
+    $('#messages').append('<p><b>'+payload.username+' says:</b> '+payload.message+' </p>');
+
+    console.log('USERNAME: '+payload.username);
+    console.log('MESSAGE: '+payload.message);
+
+});
+
+
+function send_message(){
+    var payload = {};
+    payload.room = chat_room;
+    payload.username = username;
+    payload.message = $('#send_message_holder').val();
+    console.log('*** Client Log Message: \'send_message\' payload: '+JSON.stringify(payload));
+    socket.emit('send_message', payload);
+}
+
+
+
+
+
+//Send message that we are interested in joining a room
+$(function(){
+    var payload = {};
+    payload.room = chat_room;
+    payload.username = username;
+
+    console.log('*** Client Log Message: \'join_room\' payload: '+JSON.stringify(payload));
+    socket.emit('join_room', payload);
 });
